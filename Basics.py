24713a760215db5fd1310a6936918856fbbde49c
Pooja <79531660+pooja2434@users.noreply.github.com>
2025-01-05 22:47:19 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -7,3 +7,33 @@ type(name)
 
 #First letters in Caps
 name.title()
+#Capital letter
+name.upper()
+#small letter
+name.lower()
+
+#f string
+first_name='Pooja'
+last_name='Ravi'
+full_name=f' Hi {first_name} {last_name}'
+print(full_name)
+
+print("python")
+#One tab
+print(" \t python")
+
+# One tab #Next line
+#One tab
+print(" \n \t python")
+
+# One tab #Next line
+print("Languages: \n\t Python \n\t SQL \n\t Java")
+
+language='Python '
+# To see the length of the string
+# as it counts space too - The o\p is 7
+len(language)
+# To remove the left spaces
+language.lstrip()
+# To remove the right spaces
+language.rstrip()
