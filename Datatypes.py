1f2fce4d37fa0ac6fe3a98290dba28b80bc2e1fb
Pooja <79531660+pooja2434@users.noreply.github.com>
2025-01-06 00:37:05 +0530

    Update Datatypes.py

--- Datatypes.py
+++ Datatypes.py
@@ -1,1 +1,13 @@
+List
+A list is a data structure in Python that is a mutable, or changeable, ordered sequence of elements. lists are defined by having values between square brackets [ ] .
+
+#O/p will not be added between list instead it brings all the values in one list
+L1=[1,2,3]
+L2=[4,5,6]
+L1+L2
+
+#Inserts in second position
+L1.insert(2,10)
+L1
+
 
