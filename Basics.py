177fd3305cd8c8173246d89128768e96658b3f60
Pooja <79531660+pooja2434@users.noreply.github.com>
2025-01-05 22:17:16 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -1,13 +1,16 @@
 import os
-
 # Pandas for reading file and cleaning data
 import pandas as pd
-
 #Getting the current working directory
 os.getcwd()
-
 #Changing the directory
 os.chdir("C://Users//Pooja//Dataset")
-
 # Reading a csv file
 Admit =pd.read_csv("admit.csv")
+# To see first 5 rows 
+Admit.head()
+#To see the last 5 rows
+Admit.tail()
+# To display only two rows
+pd.set_option('display.max_rows',2)
+Admit
